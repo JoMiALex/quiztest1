6a6b706bdd61064f38e111981af31551d903792a
JoMiAlex <lottjohnmichael@yahoo.com>
2025-09-05 23:16:24 -0700

    first commit

---
+++ src/main/java/com/jmlott/quiztest1/Quiztest1Application.java
@@ -0,0 +1,13 @@
+package com.jmlott.quiztest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiztest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiztest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmlott/quiztest1/Quiztest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmlott.quiztest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiztest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
